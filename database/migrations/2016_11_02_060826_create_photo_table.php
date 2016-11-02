df70e4c845a6ff5d26901ff6dee1c42d4623eb2a
Chris Harris <c.harris@communicationinnovations.com.au>
2016-11-02 16:08:50 +1000

    new migrations

---
+++ database/migrations/2016_11_02_060826_create_photo_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreatePhotoTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('photo', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->binary('content', 16777215);
+			$table->string('rel_type')->nullable();
+			$table->integer('rel_id')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('photo');
+	}
+
+}
---
+++ database/migrations/2016_11_02_060826_create_staff_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateStaffTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('staff', function(Blueprint $table)
+		{
+			$table->integer('id', true);
+			$table->string('name');
+			$table->integer('partner_id')->nullable();
+			$table->integer('photo_id')->nullable();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('staff');
+	}
+
+}
